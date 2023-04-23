cedb3f2eb7d344ae97a16706291b8410d0c6d85f
Sruthi23Yaradi <131550098+Sruthi23Yaradi@users.noreply.github.com>
2023-04-23 20:48:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let grid1 = document.querySelsctor("#icon");
+let grid2 = document.querySelsctor("#icon1");
+let grid3 = document.querySelsctor("#icon2");
+let grid4 = document.querySelsctor("#icon3");
+let grid5 = document.querySelsctor("#icon4");
+let grid6 = document.querySelsctor("#icon5");
+let grid7 = document.querySelsctor("#icon6");
+let grid8 = document.querySelsctor("#icon7");
+let grid9 = document.querySelsctor("#icon8");
+let grid10 = document.querySelsctor("#icon9");
+let grid11 = document.querySelsctor("#icon10");
+let grid12 = document.querySelsctor("#icon11");
+let grid13 = document.querySelsctor("#icon12");
+let grid14 = document.querySelsctor("#icon13");
+let grid15 = document.querySelsctor("#icon14");
+
+grid1.addEventListener('click',() => grid1.style.backgroundColor='#337ab7')
+grid2.addEventListener('click',() => grid2.style.backgroundColor='#337ab7')
+grid3.addEventListener('click',() => grid3.style.backgroundColor='#337ab7')
+grid4.addEventListener('click',() => grid4.style.backgroundColor='#337ab7')
+grid5.addEventListener('click',() => grid5.style.backgroundColor='#337ab7')
+grid6.addEventListener('click',() => grid6.style.backgroundColor='#337ab7')
+grid7.addEventListener('click',() => grid7.style.backgroundColor='#337ab7')
+grid8.addEventListener('click',() => grid8.style.backgroundColor='#337ab7')
+grid9.addEventListener('click',() => grid9.style.backgroundColor='#337ab7')
+grid10.addEventListener('click',() => grid10.style.backgroundColor='#337ab7')
+grid11.addEventListener('click',() => grid11.style.backgroundColor='#337ab7')
+grid12.addEventListener('click',() => grid12.style.backgroundColor='#337ab7')
+grid13.addEventListener('click',() => grid13.style.backgroundColor='#337ab7')
+grid14.addEventListener('click',() => grid14.style.backgroundColor='#337ab7')
+grid15.addEventListener('click',() => grid15.style.backgroundColor='#337ab7')
